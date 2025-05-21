35af8da1f60e463d756c0d185ad1b9dd8b5e51d8
Ebrahim Fadly <96057410+MerSake@users.noreply.github.com>
2025-05-21 20:49:42 +0300

    added more models, modified db connection

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,13 @@
 import os
 from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
 from dotenv import load_dotenv
+from sqlalchemy.orm import sessionmaker
+
 
 load_dotenv()
 db = create_engine(os.getenv("sqlite_url"))
+Base = declarative_base()
+
+
+LocalSession = sessionmaker(autocommit=False, autoflush=False, bind=db)
--- app/models.py
+++ app/models.py
@@ -1,16 +1,44 @@
-from . import db
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
+from datetime import datetime
+from . import Base
 
 
-class reader(db.Model):
-    __tablename__ = "reader"
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), nullable=False)
-    email = db.Column(db.String(50), unique=True, nullable=False)
-    books = db.relationship("book", backref="reader", lazy=True)
+class Librarian(Base):
+    __tablename__ = "librarians"
+    id = Column(Integer, primary_key=True)
+    email = Column(String(50), unique=True, nullable=False)
+    password = Column(String(50), nullable=False)
 
 
-class librarian(db.Model):
-    __tablename__ = "librarian"
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(50), unique=True, nullable=False)
-    password = db.Column(db.String(50), nullable=False)
+class Reader(Base):
+    __tablename__ = "readers"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50), nullable=False)
+    email = Column(String(50), unique=True, nullable=False)
+
+    borrowed_books = relationship("BorrowedBook", back_populates="reader")
+
+
+class Book(Base):
+    __tablename__ = "books"
+    id = Column(Integer, primary_key=True)
+    title = Column(String(255), nullable=False)
+    author = Column(String(100), nullable=False)
+    year = Column(Integer)
+    isbn = Column(String(50), unique=True)
+    copies_available = Column(Integer, nullable=False, default=1)
+
+    borrowed_books = relationship("BorrowedBook", back_populates="book")
+
+
+class BorrowedBook(Base):
+    __tablename__ = "borrowed_books"
+    id = Column(Integer, primary_key=True)
+    book_id = Column(Integer, ForeignKey("books.id"), nullable=False)
+    reader_id = Column(Integer, ForeignKey("readers.id"), nullable=False)
+    borrow_date = Column(DateTime, default=datetime.date)
+    return_date = Column(DateTime, nullable=True)
+
+    book = relationship("Book", back_populates="borrowed_books")
+    reader = relationship("Reader", back_populates="borrowed_books")
