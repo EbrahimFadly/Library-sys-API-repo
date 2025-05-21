08cf21b0869ebe3d60bf9f5146d73d73a19f0dd5
Ebrahim Fadly <96057410+MerSake@users.noreply.github.com>
2025-05-21 21:19:32 +0300

    initiate db and add sample data

---
+++ app/seeder.py
@@ -0,0 +1,64 @@
+from sqlalchemy.orm import Session, sessionmaker
+from sqlalchemy import create_engine
+from .models import Base, Librarian, Reader, Book
+from passlib.hash import bcrypt
+import os
+from dotenv import load_dotenv
+
+
+# Sample data
+sample_librarians = [
+    Librarian(email="admin1@example.com", password=bcrypt.hash("password123")),
+    Librarian(email="admin2@example.com", password=bcrypt.hash("pass1234")),
+]
+
+sample_readers = [
+    Reader(name="Alice Smith", email="alice@example.com"),
+    Reader(name="Bob Jones", email="bob@example.com"),
+]
+
+sample_books = [
+    Book(
+        title="The Psychology of Prejudice: From Attitudes to Social Action",
+        author="Lynne M. Jackson",
+        year=2019,
+        isbn="1234567890",
+        copies_available=3,
+    ),
+    Book(
+        title="Brave New World",
+        author="Aldous Huxley",
+        year=1932,
+        isbn="0987654321",
+        copies_available=0,
+    ),
+]
+
+
+def seed_data():
+    load_dotenv()
+    db = create_engine(os.getenv("sqlite_url"))
+    LocalSession = sessionmaker(autocommit=False, autoflush=False, bind=db)
+
+    # Create all tables
+    Base.metadata.create_all(bind=db)
+    db = LocalSession()
+    try:
+        # Clear old data
+        # db.query(Book).delete()
+        # db.query(Reader).delete()
+        # db.query(Librarian).delete()
+        # db.commit()
+
+        # Add new data
+        db.add_all(sample_librarians)
+        db.add_all(sample_readers)
+        db.add_all(sample_books)
+        db.commit()
+        print("data inserted successfully.")
+    finally:
+        db.close()
+
+
+if __name__ == "__main__":
+    seed_data()
